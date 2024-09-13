0ca767a0405aedcfc9b4b522f5cf242edc60b31a
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-13 16:38:46 -0400

    Instalacion de material UI y configuracion de MUI con Vite

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,9 +1,12 @@
+import { Typography } from "@mui/material";
 
 export const JournalPage = () => {
   return (
-    <div>
-      JournalPage
-    </div>
+    <>
+      <Typography variant='h1'>
+        JournalPage
+      </Typography>     
+    </>
   )
 }
 
