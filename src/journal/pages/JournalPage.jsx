ef83f2884cf1097be73821b63400ec4fe63a8edd
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-16 13:48:37 -0400

    Creacion de la una vista llamada NothingSlectedView

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,11 +1,13 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Irure in laboris consectetur id nulla reprehenderit ea ipsum laboris occaecat. Nulla in labore nulla dolore et aliquip officia qui excepteur proident sit. Ex sit eiusmod ullamco ullamco nulla enim nulla laboris id aute Lorem id fugiat. Ut consequat excepteur adipisicing consectetur sit magna aliquip velit nisi et mollit non.</Typography> 
+      {/* <Typography>Irure in laboris consectetur id nulla reprehenderit ea ipsum laboris occaecat. Nulla in labore nulla dolore et aliquip officia qui excepteur proident sit. Ex sit eiusmod ullamco ullamco nulla enim nulla laboris id aute Lorem id fugiat. Ut consequat excepteur adipisicing consectetur sit magna aliquip velit nisi et mollit non.</Typography>  */}
        {/*nothing selected  */}
+       <NothingSelectedView />
        {/* note view */}
     </JournalLayout>
   )
