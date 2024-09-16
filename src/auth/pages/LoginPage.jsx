023d85667aa0a7c53ee083375aecc1178726b744
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-16 11:20:28 -0400

    Creacion de un componente AuthLayout para el uso en futuras vistas de register y demas

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,23 +1,11 @@
 import {Link as RouterLink} from 'react-router-dom';
 import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
+import { AuthLayout } from '../layout/AuthLayout';
 
 export const LoginPage = () => {
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{minHeight:'100vh', backgroundColor: 'primary.main', padding: 4}}
-    >
-        <Grid item
-          className= 'box-shadow'
-          xs = {3}
-          sx={{backgroundColor: 'white', padding: 3, borderRadius: 2}}
-        >
-          <Typography variant='h5' sx={{mb:1}}>Login</Typography>
+    <AuthLayout title='Login'>
           <form>
             <Grid container>
                 <Grid item xs={12} sx={{mt:2}}>
@@ -56,9 +44,7 @@ export const LoginPage = () => {
               </Grid>
             </Grid>
           </form>
-
-        </Grid>
-
-    </Grid>
+    </AuthLayout>
+  
   )
 }
