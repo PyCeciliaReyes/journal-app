b31cfb144a062bb81e399852ffec6a2d49dcdf08
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-16 11:59:06 -0400

    Creacion del NavBar, configuracion de los tamaños y detalles segun tipo de pantalla

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material"
+import { NavBar } from "../components";
 
 const drawerWidth = 240;
 export const JournalLayout = ({children}) => {
   return (
     <Box sx={{display:'flex'}}>
         {/* navbar */}
+        <NavBar drawerWidth={ drawerWidth } />
         {/* sidebar */}
         <Box
             component='main'
