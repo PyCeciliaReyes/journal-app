d578a877c43020f65de873da46a0ec47b0634a45
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-17 16:13:41 -0400

    configuracion de providers.js y se agrega el config de dichos providers al thunks

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { singInWithGoogle } from '../../firebase/providers';
 import {checkingCredentials} from './authSlice';
 
 export const checkingAuthentication = (email, password) => {
@@ -9,5 +10,7 @@ export const checkingAuthentication = (email, password) => {
 export const startGoogleSignIn = () => {
     return async(dispatch) => {
         dispatch(checkingCredentials());
+        const result = await singInWithGoogle();
+        console.log(result)
     }
 }
