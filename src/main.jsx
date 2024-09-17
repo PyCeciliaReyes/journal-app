92c5b9dbe6d851f794b615398771ba47ebf1e72f
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-17 14:33:09 -0400

    Instalacion de reduxjs/toolkit y configuracion inicial del store, se agrega el provider en el main para recibir los estados

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from 'react-redux';
+
 import {JournalApp} from './JournalApp.jsx'
+import { store } from './store';
+
 import './styles.css'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>,
 )
