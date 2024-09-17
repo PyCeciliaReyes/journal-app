349d665261b447201b53b57bd3ee63f311e17b63
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-17 17:02:29 -0400

    Formulario de registro de usuarios
    Se configura los cambios utilizando el custom hook useForm() para cambiar los estados del formulario de registro

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,12 +1,26 @@
 import {Link as RouterLink} from 'react-router-dom';
-import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
 import { AuthLayout } from '../layout/AuthLayout';
+import { useForm } from '../../hooks';
+
+const formData = {
+  email: 'cecilia@google.com',
+  password: '12345',
+  displayName: 'Cecilia Reyes',
+}
 
 export const RegisterPage = () => {
+
+  const { email, password, onInputChange, displayName, formState } = useForm(formData);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(formState)
+  }
+
   return (
     <AuthLayout title='Crear cuenta'>
-          <form>
+          <form onSubmit={onSubmit} >
             <Grid container>
                 <Grid item xs={12} sx={{mt:2}}>
                   <TextField 
@@ -14,6 +28,9 @@ export const RegisterPage = () => {
                     type="text"
                     placeholder="Nombre completo"
                     fullWidth
+                    name='displayName'
+                    value={displayName}
+                    onChange={onInputChange}
                   />
                 </Grid>
                 <Grid item xs={12} sx={{mt:2}}>
@@ -22,6 +39,9 @@ export const RegisterPage = () => {
                     type="email"
                     placeholder="correo@google.com"
                     fullWidth
+                    name='email'
+                    value={email}
+                    onChange={onInputChange}
                   />
                 </Grid>
                 <Grid item xs={12} sx={{mt:2}}>
@@ -30,11 +50,14 @@ export const RegisterPage = () => {
                     type="password"
                     placeholder="Contraseña"
                     fullWidth
+                    name='password'
+                    value={password}
+                    onChange={onInputChange}
                   />
               </Grid>
               <Grid container spacing={2} sx={{mb:2, mt: 1}}>
                   <Grid item xs={12} >
-                      <Button variant="contained" fullWidth>Crear cuenta</Button>
+                      <Button type='submit' variant="contained" fullWidth>Crear cuenta</Button>
                   </Grid>
               </Grid>
               <Grid container direction='row' justifyContent='end'>
