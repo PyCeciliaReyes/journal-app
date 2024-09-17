020aeb6c5f677ce6e10e9bdfffb76d22f5b42084
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-17 16:42:16 -0400

    Disparar las acciones de autenticacion y se deshabilita momentaneamente los botones cuando esta en el status de 'checking'

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
-import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {Link as RouterLink} from 'react-router-dom';
 import { Google } from "@mui/icons-material";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
@@ -9,12 +10,15 @@ import { checkingAuthentication, startGoogleSignIn } from '../../store/auth';
 
 
 export const LoginPage = () => {
+  const {status} = useSelector(state => state.auth);
   const dispatch = useDispatch();
 
   const { email, password, onInputChange } = useForm({
     email: 'cecilia@google.com',
     password: '12345'
-  })
+  });
+
+  const isAuthenticating = useMemo(() => status === 'checking', [status]);
 
   const onSubmit = (event) => {
     event.preventDefault();
@@ -56,10 +60,17 @@ export const LoginPage = () => {
               </Grid>
               <Grid container spacing={2} sx={{mb:2, mt: 1}}>
                   <Grid item xs={12} sm={6}>
-                      <Button variant="contained" fullWidth type='submit'>Login</Button>
+                      <Button 
+                        disabled= {isAuthenticating}
+                        variant="contained" 
+                        fullWidth 
+                        type='submit'
+                      >
+                        Login
+                      </Button>
                   </Grid>
                   <Grid item xs={12} sm={6}>
-                      <Button onClick={onGoogleSignIn} variant="contained" fullWidth>
+                      <Button  disabled={isAuthenticating} onClick={onGoogleSignIn} variant="contained" fullWidth>
                         <Google />
                           <Typography sx={{ml:1}}>
                             Google
--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,5 +1,5 @@
 import { singInWithGoogle } from '../../firebase/providers';
-import {checkingCredentials} from './authSlice';
+import {checkingCredentials, login, logout} from './authSlice';
 
 export const checkingAuthentication = (email, password) => {
     return async(dispatch) => {
@@ -11,6 +11,8 @@ export const startGoogleSignIn = () => {
     return async(dispatch) => {
         dispatch(checkingCredentials());
         const result = await singInWithGoogle();
-        console.log(result)
+        //console.log(result)
+        if (!result.ok) return dispatch(logout(result.errorMessage));
+        dispatch(login(result));
     }
 }
