0152698070d6ab53c15aaf4c48693b2fd1fda623
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-18 15:52:43 -0400

    Proceso completo de Checking Authentication

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,14 +1,36 @@
-import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { FirebaseAuth } from "../firebase/config";
+import { login, logout } from "../store/auth";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui";
+import { useCheckAuth } from "../hooks";
 
 export const AppRouter = () => {
-  return (
+    
+    const {status} = useCheckAuth();
+
+    if (status === 'checking') {
+        return <CheckingAuth/>
+    }
+    
+    return (
         <Routes>
+
+            {
+                (status === 'authenticated')
+                    ? <Route path="/*" element={<JournalRoutes/>} />
+                    : <Route path="/auth/*" element={<AuthRoutes/>} />
+            }
+            <Route path="/*" element={ <Navigate to='/auth/login' />} />
+
             {/* login y registro */}
-            <Route path="/auth/*" element={<AuthRoutes/>} />
+            {/* <Route path="/auth/*" element={<AuthRoutes/>} /> */}
             {/* journal app */}
-            <Route path="/*" element={<JournalRoutes/>} />
+            {/* <Route path="/*" element={<JournalRoutes/>} /> */}
         </Routes> 
     )
 }
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const authSlice = createSlice({
    name: 'auth',
    initialState: {
-       status: 'not-authenticated', //not-authenticated, authenticated, checking
+       status: 'checking', //not-authenticated, authenticated, checking
        uid: null,
        email: null,
        displayName: null,
