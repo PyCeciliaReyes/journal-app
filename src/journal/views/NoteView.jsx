82a87d2b74b3ae37e07a69dd2c5082877fcf59ee
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-19 11:56:24 -0400

    NoteView: utilizando el custom hool useForm se puede activar una nota para su edicion, el campo title, body. Y tambien visualizamos la fecha
    useForm: se agrega un2do useEffect que da la funcion de recomponer cada dato, ya que depende de la data de initialForm
    
    LoginPage: se agrega una variable que contiene el initial form para evitar que el useEffect se renderiza a cada rato y no traer los warning

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,42 +1,59 @@
 import { SaveOutlined } from "@mui/icons-material";
 import { Button, Grid, TextField, Typography } from "@mui/material";
 import { ImageGallery } from "../components";
+import { useForm } from "../../hooks/useForm";
+import { useSelector } from "react-redux";
+import { useMemo } from "react";
 
 
 export const NoteView = () => {
-  return (
-    <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{mb: 1}} className="animate__animated animate__fadeIn animate__faster">
-        <Grid item>
-            <Typography fontSize={39} fontWeight='light'>16 de septiembre 2024</Typography>
-        </Grid>
-        <Grid item>
-            <Button color="primary" sx={{padding: 2}}>
-                <SaveOutlined sx={{fontSize: 30, mr: 1}}/>
-                Guardar
-            </Button>
-        </Grid>
-        <Grid container>
-            <TextField 
-                type="text"
-                variant="filled"
-                fullWidth
-                placeholder="Ingresa un titulo"
-                label="Titulo"
-                sx={{border:'none', mb: 1}}
-            />
+    const { active:note } = useSelector(state => state.journal);
+    const { body, title, date, onInputChange, formState } = useForm(note);
 
-            <TextField 
-                type="text"
-                variant="filled"
-                fullWidth
-                multiline
-                placeholder="¿Que sucedio en el dia de hoy?"
-                minRows={5}
-            />
+    const dateString = useMemo(() => {
+        const newDate = new Date(date);
+        return newDate.toUTCString();
+    }, [date])
+
+    return (
+        <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{mb: 1}} className="animate__animated animate__fadeIn animate__faster">
+            <Grid item>
+                <Typography fontSize={39} fontWeight='light'>{dateString}</Typography>
+            </Grid>
+            <Grid item>
+                <Button color="primary" sx={{padding: 2}}>
+                    <SaveOutlined sx={{fontSize: 30, mr: 1}}/>
+                    Guardar
+                </Button>
+            </Grid>
+            <Grid container>
+                <TextField 
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    placeholder="Ingresa un titulo"
+                    label="Titulo"
+                    sx={{border:'none', mb: 1}}
+                    name="title"
+                    value={title}
+                    onChange={onInputChange}
+                />
+
+                <TextField 
+                    type="text"
+                    variant="filled"
+                    fullWidth
+                    multiline
+                    placeholder="¿Que sucedio en el dia de hoy?"
+                    minRows={5}
+                    name="body"
+                    value={body}
+                    onChange={onInputChange}
+                />
+            </Grid>
+            {/* galeria de imagenes */}
+            <ImageGallery />
         </Grid>
-        {/* galeria de imagenes */}
-        <ImageGallery />
-    </Grid>
   )
 }
 
