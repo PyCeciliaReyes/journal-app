e10fe8b4338059baee396e07580a2d62f43bdb77
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-20 09:39:39 -0400

    Se agrega la funcion de seleccionar multiples archivos desde React

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { SaveOutlined } from "@mui/icons-material";
-import { Button, Grid, TextField, Typography } from "@mui/material";
+import { SaveOutlined, UploadFileOutlined } from "@mui/icons-material";
+import { Button, Grid, IconButton, TextField, Typography } from "@mui/material";
 import Swal from "sweetalert2";
 import 'sweetalert2/dist/sweetalert2.css';
 
@@ -21,6 +21,8 @@ export const NoteView = () => {
         return newDate.toUTCString();
     }, [date])
 
+    const fileInputRef = useRef();
+
     useEffect(() => {
         dispatch(setActiveNote(formState));
     },[formState])
@@ -35,12 +37,26 @@ export const NoteView = () => {
         dispatch(startSaveNote());
     }
 
+    const onFileInputChange = ({target}) => {
+        if (target.files === 0) return;
+        console.log('subiendo archivos');
+
+        //dispatch(startUploadingFiles(target.files));
+    }
+
     return (
         <Grid container direction='row' justifyContent='space-between' alignItems='center' sx={{mb: 1}} className="animate__animated animate__fadeIn animate__faster">
             <Grid item>
                 <Typography fontSize={39} fontWeight='light'>{dateString}</Typography>
             </Grid>
             <Grid item>
+
+                <input ref={fileInputRef} type="file" multiple onClick={ onFileInputChange } style={{display:'none'}}/>
+
+                <IconButton color="primary" disabled={isSaving} onClick={ () => fileInputRef.current.click() } >
+                    <UploadFileOutlined/>
+                </IconButton>
+
                 <Button
                     disabled={ isSaving }
                     onClick={ onSaveNote } 
