0ae36c5733a5c0550de901865e4e4147f896042e
PyCeciliaReyes <cecibreyes@gmail.com>
2024-09-20 17:16:23 -0400

    Se utiliza componentes de materialUI para pantallas moviles

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,19 +1,43 @@
+import { useState } from "react";
 import { useSelector } from "react-redux";
-import { Box, Divider, Drawer, List, Toolbar, Typography } from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import { Box, Divider, Drawer, IconButton, List, Toolbar, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { SideBarItem } from './SideBarItem';
 
 export const SideBar = ({drawerWidht=240}) => {
     const {displayName} = useSelector( state => state.auth)
     const {notes} = useSelector( state => state.journal)
 
+    const [mobileOpen, setMobileOpen] = useState(false);
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+    
+    const handleDrawerToggle = () => {
+        
+        setMobileOpen(!mobileOpen);
+    };
+
     return (
         <Box 
             component='nav'
             sx={{width:{sm:drawerWidht}, flexShrink: {sm:0}}}
         >
+            
+             {isMobile && !mobileOpen && (
+                <IconButton 
+                    color="inherit" 
+                    aria-label="open drawer" 
+                    edge="start" 
+                    onClick={handleDrawerToggle}
+                    sx={{ position: 'absolute', left: 0, top: 0, zIndex: 1300 }}
+                >
+                    <MenuIcon color="primary" />
+                </IconButton>
+            )}
             <Drawer
-                variant="permanent"
-                open
+                variant={isMobile ? 'temporary' : 'permanent'}
+                open={isMobile ? mobileOpen : true}
+                onClose={handleDrawerToggle}
                 sx={{
                     display: {xs:'block'},
                     '& .MuiDrawer-paper': {boxSizing:'border-box', width: drawerWidht}
